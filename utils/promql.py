eba951d9ef24ca8f54d8fb2b2bb421e2bd27dccc
Balkrishna Pandey <pandeybk@users.noreply.github.com>
2023-07-24 16:03:46 -0600

    fetch data in a regular interval, logging added to show the progress
    Signed-off-by: Balkrishna Pandey <pandeybk@users.noreply.github.com>

--- utils/promql.py
+++ utils/promql.py
@@ -3,16 +3,40 @@ import requests
 import yaml
 import urllib3
 from datetime import datetime, timedelta
+import os
+import logging
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+scheduler = BlockingScheduler()
+
+# Create a custom logger
+logger = logging.getLogger(__name__)
+
+# Set the level of logger to INFO
+logger.setLevel(logging.INFO)
+
+# Create handlers
+c_handler = logging.StreamHandler()
+c_handler.setLevel(logging.INFO)
+
+# Create formatters and add it to handlers
+c_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+c_handler.setFormatter(c_format)
+
+# Add handlers to the logger
+logger.addHandler(c_handler)
 
 # Load your YAML file
 with open("queries.yaml", "r") as f:
     queries = yaml.safe_load(f)
+    logger.info("Successfully loaded the YAML file.")
 
 # Setup Prometheus connection
 prometheus_url = "https://prometheus-k8s-openshift-monitoring.apps.hubztp.telco.ocp.run"
 token = "sha256~fiNsYzfBum-76aC6hS3auH_kNzG3xkYqrSsKXsiY3Lc"
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+logger.info("Disabled urllib3 warnings.")
 
 headers = {
     "Authorization": "Bearer " + token,
@@ -25,31 +49,41 @@ csv_headers = ["timestamp"] + [q["metricName"] for q in queries]
 data = {}
 
 # Fetch the data for each query
-for query in queries:
-    response = requests.get(
-        prometheus_url + "/api/v1/query",
-        params={"query": query["query"], "time": round(datetime.now().timestamp())},
-        headers=headers,
-        verify=False  # If the Prometheus instance uses self-signed SSL
-    )
-    response.raise_for_status()
+def fetch_data():
+    logger.info("Fetching data from Prometheus.")
+    for query in queries:
+        response = requests.get(
+            prometheus_url + "/api/v1/query",
+            params={"query": query["query"], "time": round(datetime.now().timestamp())},
+            headers=headers,
+            verify=False  # If the Prometheus instance uses self-signed SSL
+        )
+        response.raise_for_status()
+        logger.info(f"Fetched data for query {query['query']}.")
 
-    result = response.json()["data"]["result"]
-    if result:
-        value = result[0]["value"]
-        timestamp = value[0]
-        # If this timestamp is new, initialize it with a list of None values
-        if timestamp not in data:
-            data[timestamp] = [None]*len(queries)
-        # Add the metric value to the list at the position determined by the order of queries in the YAML file
-        data[timestamp][csv_headers.index(query["metricName"])-1] = value[1]
+        result = response.json()["data"]["result"]
+        if result:
+            value = result[0]["value"]
+            timestamp = value[0]
+            # If this timestamp is new, initialize it with a list of '0.0' values
+            if timestamp not in data:
+                data[timestamp] = ['0.0']*len(queries)
+                # Add the metric value to the list at the position determined by the order of queries in the YAML file
+                data[timestamp][csv_headers.index(query["metricName"])-1] = value[1]
 
+                # Prepare data for writing to CSV
+                rows = [[timestamp] + values for timestamp, values in data.items()]
 
-# Prepare data for writing to CSV
-rows = [[timestamp] + values for timestamp, values in data.items()]
+                # Append the new data to CSV
+                with open("metrics.csv", "a", newline="") as f:
+                    writer = csv.writer(f)
+                    writer.writerow(rows[-1])
+                logger.info(f"Appended data for timestamp {timestamp} to CSV file.")
 
-# Write the data to CSV
-with open("metrics.csv", "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(csv_headers)
-    writer.writerows(rows)
+# Schedule the task to run every 10 seconds (or whatever your desired interval is)
+scheduler.add_job(fetch_data, 'interval', seconds=10)
+logger.info("Scheduled the job to run every 10 seconds.")
+
+# Start the scheduler
+logger.info("Starting the scheduler.")
+scheduler.start()
