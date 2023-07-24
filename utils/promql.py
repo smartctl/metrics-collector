2968d72316e40143f490011e8f11434c5a29999c
Balkrishna Pandey <pandeybk@users.noreply.github.com>
2023-07-24 17:55:30 -0600

    pod name added
    Signed-off-by: Balkrishna Pandey <pandeybk@users.noreply.github.com>

--- utils/promql.py
+++ utils/promql.py
@@ -2,12 +2,11 @@ import csv
 import requests
 import yaml
 import urllib3
-from datetime import datetime, timedelta
 import os
 import logging
-from apscheduler.schedulers.blocking import BlockingScheduler
-
-scheduler = BlockingScheduler()
+from apscheduler.schedulers.background import BackgroundScheduler
+import time
+from kubernetes import client, config
 
 # Create a custom logger
 logger = logging.getLogger(__name__)
@@ -26,8 +25,15 @@ c_handler.setFormatter(c_format)
 # Add handlers to the logger
 logger.addHandler(c_handler)
 
+def get_app_names():
+    config.load_kube_config()
+    v1 = client.CoreV1Api()
+    namespace = "openshift-monitoring"
+    ret = v1.list_namespaced_pod(namespace=namespace)
+    return [i.metadata.name for i in ret.items]
+
 # Load your YAML file
-with open("queries.yaml", "r") as f:
+with open("queries2.yaml", "r") as f:
     queries = yaml.safe_load(f)
     logger.info("Successfully loaded the YAML file.")
 
@@ -42,49 +48,74 @@ headers = {
     "Authorization": "Bearer " + token,
 }
 
-# Prepare to fetch the data
-csv_headers = ["timestamp"] + [q["metricName"] for q in queries]
+def fetch_data(namespace):
+    app_names = get_app_names()
+    for app_name in app_names:
+        logger.info(f"Fetching data for app: {app_name}, namespace: {namespace}.")
+        # Initialize a dictionary that maps timestamps to lists of metric values
+        data = {}
+        # Fetch the data for each query
+        for query in queries:
+            # formatted_query = query["query"].format(namespace=namespace, app_name=app_name)
+            formatted_query = query["query"].replace("{namespace}", namespace).replace("{app_name}", app_name)
 
-# Initialize a dictionary that maps timestamps to lists of metric values
-data = {}
 
-# Fetch the data for each query
-def fetch_data():
-    logger.info("Fetching data from Prometheus.")
-    for query in queries:
-        response = requests.get(
-            prometheus_url + "/api/v1/query",
-            params={"query": query["query"], "time": round(datetime.now().timestamp())},
-            headers=headers,
-            verify=False  # If the Prometheus instance uses self-signed SSL
-        )
-        response.raise_for_status()
-        logger.info(f"Fetched data for query {query['query']}.")
+            params = {
+                "query": formatted_query,
+                "time": round(time.time())
+            }
+            response = requests.get(
+                prometheus_url + "/api/v1/query",
+                params=params,
+                headers={"Authorization": f"Bearer {token}"},
+                verify=False  # If the Prometheus instance uses self-signed SSL
+            )
+            try:
+                response.raise_for_status()
+            except requests.exceptions.HTTPError as err:
+                logger.error(f"HTTP error occurred: {err}")
+                logger.error(f"Failed to fetch data for query {params['query']}.")
+                continue  # skip to the next query or app_name
 
-        result = response.json()["data"]["result"]
-        if result:
-            value = result[0]["value"]
-            timestamp = value[0]
-            # If this timestamp is new, initialize it with a list of '0.0' values
-            if timestamp not in data:
-                data[timestamp] = ['0.0']*len(queries)
-                # Add the metric value to the list at the position determined by the order of queries in the YAML file
-                data[timestamp][csv_headers.index(query["metricName"])-1] = value[1]
+            result = response.json()["data"]["result"]
+            if result:
+                value = result[0]["value"]
+                timestamp = value[0]
+                if timestamp not in data:
+                    data[timestamp] = [app_name] + ['0.0']*len(queries)
+                data[timestamp][queries.index(query)+1] = value[1]
 
-                # Prepare data for writing to CSV
-                rows = [[timestamp] + values for timestamp, values in data.items()]
+        # Prepare data for writing to CSV
+        rows = [[timestamp] + values for timestamp, values in sorted(data.items())]
 
-                # Append the new data to CSV
-                with open("metrics.csv", "a", newline="") as f:
-                    writer = csv.writer(f)
-                    writer.writerow(rows[-1])
-                logger.info(f"Appended data for timestamp {timestamp} to CSV file.")
+        # Write the data to CSV
+        # with open("metrics.csv", "a", newline="") as f:
+        #     writer = csv.writer(f)
+        #     writer.writerow(["timestamp", "app_name"] + [q["metricName"] for q in queries])
+        #     for row in rows:
+        #         writer.writerow(row)
+        #         logger.info(f"Appended data for timestamp {row[0]} to CSV file.")
+        with open("metrics.csv", "a", newline="") as f:
+            writer = csv.writer(f)
+            if f.tell() == 0:
+                writer.writerow(["timestamp", "app_name"] + [q["metricName"] for q in queries])
+            for row in rows:
+                writer.writerow(row)
+                logger.info(f"Appended data for timestamp {row[0]} to CSV file.")
+
 
 # Schedule the task to run every 10 seconds (or whatever your desired interval is)
-seconds=10
-scheduler.add_job(fetch_data, 'interval', seconds=seconds)
-logger.info("Scheduled the job to run every %.f seconds.", seconds)
+scheduler = BackgroundScheduler()
+
+namespace = "openshift-monitoring"  # Substitute with your namespace
+
+scheduler.add_job(fetch_data, 'interval', seconds=10, args=[namespace])
+logger.info("Scheduled the job to run every 10 seconds.")
 
 # Start the scheduler
 logger.info("Starting the scheduler.")
 scheduler.start()
+
+# Keep the script running
+while True:
+    time.sleep(1)
